5bba4f5c31d671e9414a669ec665aab634ef2398
saintno <tctien342@gmail.com>
2024-08-05 20:30:46 +0700

    fix(lib): correct lib path

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-import * as Client from "./src/client";
-import * as Wrapper from "./src/call-wrapper";
-import * as Pool from "./src/pool";
-import * as Prompt from "./src/prompt-builder";
+export { ComfyApi } from "./src/client";
+export { CallWrapper } from "./src/call-wrapper";
+export { ComfyPool, EQueueMode } from "./src/pool";
+export { PromptBuilder } from "./src/prompt-builder";
 
-import type { TSamplerName, TSchedulerName } from "./types/sampler";
-
-export { Pool, Prompt, Wrapper, Client };
-export type { TSamplerName, TSchedulerName };
+export type { TSamplerName, TSchedulerName } from "./types/sampler";
